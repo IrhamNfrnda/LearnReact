fd1692cc5a8d773c5b6efd6bc51a76bbb4ccc55d
IrhamNfrnda <inofrianda@gmail.com>
2022-10-10 23:08:16 +0700

    Update App.js

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -6,11 +6,25 @@ function App() {
   return (
     <div className="Parentbox"> 
       <FotoProduk/>
-      <ProdukInfo name="Irham" category = "Lebaran"/>
+      <ProdukInfo isDiscount="yes" name="Irham" category = "Lebaran"/>
     </div>
   );
 }
 
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+
+  if (isDiscount) {
+    return (
+      <p>Discount 50% Off</p>
+    );
+  } else {
+    return (
+      <p>Belum ada Discount</p>
+    )
+  }
+
+}
 
 function FotoProduk() {
   return (
@@ -21,23 +35,33 @@ function FotoProduk() {
 }
 
 function ProdukInfo(props) {
-  const { category, name } = props; 
+  const { category, name, isDiscount } = props; 
+  const benefits = ["Anti Api", "Tahan Banting", "Dapat dicuci dengan pasir"];
+
+  const listBenefits = benefits.map((itemBenefit) =>
+    <li> { itemBenefit }</li>
+  );
+
   return (
       <div className='Deskripsi'>
         <p className='Cate'>{ category }</p>
         <h1 className='Title'>{ name }</h1>
         <p className='Price'>IDR 73.300.999</p>
+        <CheckDiscount isDiscount = { isDiscount }/>
         <p className='Info'>
           Ipsum amet proident cupidatat et ipsum ex anim excepteur id aliquip exercitation est est. Reprehenderit aliquip sint laboris minim. Sint laborum et tempor labore occaecat proident proident enim excepteur velit pariatur. Aliquip pariatur tempor nulla elit do incididunt commodo elit. Ea non minim culpa sit mollit aliqua reprehenderit sint laborum sunt adipisicing reprehenderit. Sit eu tempor culpa Lorem.
         </p>
-        <a onClick={TambahCart} href='#'>Add to Cart</a>
+        <ul>
+          { listBenefits }
+        </ul>
+        <a onClick={(e) => TambahCart(name, e )} href='#'>Add to Cart</a>
       </div>
   );
 }
 
 
-function TambahCart() {
-  console.log("Membeli Produk...");
+function TambahCart(e) {
+  console.log("Membeli Produk " + e);
 }
 
 
